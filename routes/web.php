817756f6322b665949a51a9a3f28318c0e2f94c1
kassahundinber <kassahun1722@gmail.com>
2022-05-17 15:49:55 +0300

    Request verifiction added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 use App\Http\Controllers\GsrController;
+use App\Http\Controllers\RcvController;
 use App\Models\Gsr;
+use App\Models\Rcv;
 
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +22,8 @@ Route::get('/', function () {
 });
 Route::get('/Gsr/register', [GsrController::class, 'register'])->name('Gsr/register');
 Route::post('/Gsr/register', [GsrController::class, 'store'])->name('Gsr/register');
+Route::get('/Rcv/register', [RcvController::class, 'register'])->name('Rcv/register');
+Route::post('/Rcv/register', [RcvController::class, 'store'])->name('Rcv/register');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
@@ -28,3 +32,9 @@ Route::get('/Gsr/edit/{id}', [GsrController::class, 'edit']);
 Route::post('/Gsr/update',[GsrController::class,'update'])->name('Gsr/update');
 Route::get('/Gsr/delete/{id}', [GsrController::class, 'delete']);
 Route::get('/Gsr/search/{id}', [GsrController::class, 'search'])->name('Gsr/search');
+Route::get('/Rcv/list', [RcvController::class, 'get_all'])->name('Rcv/list');
+Route::get('/Rcv/edit/{graduateid}', [RcvController::class, 'edit']);
+Route::post('/Rcv/update',[RcvController::class,'update'])->name('Rcv/update');
+Route::get('/Rcv/delete/{graduateid}', [RcvController::class, 'delete']);
+Route::get('/Rcv/search/{graduateid}', [RcvController::class, 'search'])->name('Rcv/search');
+
